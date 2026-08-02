c9fdf0c7ed0af84674d9b8e712902ab3ef753fab
Babu S Suresh (SX/ETD3-MM) <juj2kor@bosch.com>
2026-08-02 22:18:26 +0530

    Use Azure backend for conversation APIs

--- src/services/conversationApi.js
+++ src/services/conversationApi.js
@@ -1,8 +1,8 @@
-// const API_BASE_URL =
-//   "https://mabdo-dtc3gkb3dhctg9hw.eastus-01.azurewebsites.net";
-
 const API_BASE_URL =
-  "http://127.0.0.1:8000";
+  "https://mabdo-dtc3gkb3dhctg9hw.eastus-01.azurewebsites.net";
+
+// const API_BASE_URL =
+//   "http://127.0.0.1:8000";
 
 
 export async function createConversation(
